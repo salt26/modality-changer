412dd6105cf776cda571b1acb7f4849e1b765106
Dantae An <48adt@naver.com>
2022-01-27 17:27:00 +0900

    modify english emotion category criteria

--- preprocess_english_emotion.py
+++ preprocess_english_emotion.py
@@ -1,6 +1,36 @@
+import numpy as np
 import pandas as pd
-from read_features import valence_category, arousal_category
 from build_chord_transition_matrix import category_to_index
+
+# for English_emotion data
+def valence_category(valence):
+    if isinstance(valence, (np.ndarray, np.generic)):
+        a = np.full_like(valence, "H", dtype=np.str)
+        a[valence <= 0.3325] = "M"
+        a[valence <= 0.1125] = "L"
+        return a
+    else:
+        if valence <= 0.1125:
+            return "L"
+        elif valence <= 0.3325:
+            return "M"
+        else:
+            return "H"
+
+# for English_emotion data
+def arousal_category(arousal):
+    if isinstance(arousal, (np.ndarray, np.generic)):
+        a = np.full_like(arousal, "H", dtype=np.str)
+        a[arousal < -0.1500] = "M"
+        a[arousal <= -0.3350] = "L"
+        return a
+    else:
+        if arousal <= -0.3350:
+            return "L"
+        elif arousal < -0.1500:
+            return "M"
+        else:
+            return "H"
 
 if __name__ == '__main__':
     # Amy Beth Warriner, Victor Kuperman, and Marc Brysbaert. Norms of valence, arousal, and dominance for 13,915 English lemmas. Behavior research methods 45(4): 1191–1207, 2013.
@@ -13,4 +43,4 @@ if __name__ == '__main__':
         "CategoryIndex": category_to_index(valence_category((df["V.Mean.Sum"].to_numpy() - 5) / 4.0),
             arousal_category((df["A.Mean.Sum"].to_numpy() - 5) / 4.0))}
     df2 = pd.DataFrame(data)
-    df2.to_csv("./output/NewEnglishSentiment.csv", index=False)
+    df2.to_csv("./output/EnglishSentiment2.csv", index=False)
